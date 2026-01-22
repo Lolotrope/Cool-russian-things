23bde0d0924f70acf719b2c0a7f4f5ea54e40543
Lolotrope <157278814+Lolotrope@users.noreply.github.com>
2026-01-23 00:43:50 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.menu-button').addEventListener('click', function() {
-    document.querySelector('.menu').style.display = 'block';
-});
-
-document.addEventListener('click', function(event) {
-    if (!event.target.closest('.menu') && !event.target.closest('.menu-button')) {
-        document.querySelector('.menu').style.display = 'none';
-    }
-});
